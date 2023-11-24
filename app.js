1ffac76570fb53b0e6793926e181ec907d51f9eb
Roman <mazaxaka.tyt@gmail.com>
2023-11-24 12:03:13 +0200

    fix couches slicder

--- app.js
+++ app.js
@@ -95,7 +95,9 @@ $(document).ready(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         dots: true,
-        infinite: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
     });
 });
 
