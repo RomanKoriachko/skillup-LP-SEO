a0954031c6371edcf5b1eb13301b1a4dd3592b55
Roman <mazaxaka.tyt@gmail.com>
2023-11-29 12:51:59 +0200

    add services section adaptive

--- app.js
+++ app.js
@@ -2,6 +2,82 @@
 
 AOS.init();
 
+// Navigation
+
+const registrationBtns = document.querySelectorAll(".registration-btn");
+const form1Section = document.querySelector(".form-1-section");
+
+for (let i = 0; i < registrationBtns.length; i++) {
+    registrationBtns[i].addEventListener("click", () => {
+        window.scrollTo({
+            top: form1Section.getBoundingClientRect().top + window.scrollY,
+            behavior: "smooth",
+        });
+    });
+}
+
+const aboutBtn = document.querySelector(".header-menu-item-about");
+const programBtn = document.querySelector(".header-menu-item-program");
+const coachesBtn = document.querySelector(".header-menu-item-coaches");
+const reviewBtn = document.querySelector(".header-menu-item-review");
+const contactsBtn = document.querySelector(".header-menu-item-contact");
+
+const aboutSection = document.querySelector(".benefits-section");
+const programSection = document.querySelector(".cource-section");
+const coachesSection = document.querySelector(".coaches-section");
+const reviewSection = document.querySelector(".video-section");
+const contactSection = document.querySelector(".footer");
+
+aboutBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: aboutSection.getBoundingClientRect().top + window.scrollY,
+        behavior: "smooth",
+    });
+});
+programBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: programSection.getBoundingClientRect().top + window.scrollY,
+        behavior: "smooth",
+    });
+});
+coachesBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: coachesSection.getBoundingClientRect().top + window.scrollY,
+        behavior: "smooth",
+    });
+});
+reviewBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: reviewSection.getBoundingClientRect().top + window.scrollY,
+        behavior: "smooth",
+    });
+});
+contactsBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: contactSection.getBoundingClientRect().top + window.scrollY,
+        behavior: "smooth",
+    });
+});
+
+// Animated numbers
+
+$(".counts").each(function (index, element) {
+    var count = $(this),
+        zero = { val: 0 },
+        num = count.data("number"),
+        split = (num + "").split("."),
+        decimals = split.length > 1 ? split[1].length : 0;
+
+    gsap.to(zero, {
+        val: num,
+        duration: 3,
+        scrollTrigger: element,
+        onUpdate: function () {
+            count.text(zero.val.toFixed(decimals));
+        },
+    });
+});
+
 // phone input
 
 function validatePhone(id, formSelector) {
@@ -56,10 +132,10 @@ function validatePhone(id, formSelector) {
     input.addEventListener("keyup", reset);
 }
 
-validatePhone("#phone1", "#form-1");
-validatePhone("#phone2", "#form-2");
-validatePhone("#phone3", "#form-3");
-validatePhone("#phone4", "#form-4");
+// validatePhone("#phone1", "#form-1");
+// validatePhone("#phone2", "#form-2");
+// validatePhone("#phone3", "#form-3");
+// validatePhone("#phone4", "#form-4");
 
 // Show cource items
 
@@ -145,82 +221,6 @@ function changeVideo(videoNumber) {
     }
     mainVideo.src = `https://www.youtube.com/embed/${videoNumber}`;
 }
-
-// Navigation
-
-const registrationBtns = document.querySelectorAll(".registration-btn");
-const form1Section = document.querySelector(".form-1-section");
-
-for (let i = 0; i < registrationBtns.length; i++) {
-    registrationBtns[i].addEventListener("click", () => {
-        window.scrollTo({
-            top: form1Section.getBoundingClientRect().top + window.scrollY,
-            behavior: "smooth",
-        });
-    });
-}
-
-const aboutBtn = document.querySelector(".header-menu-item-about");
-const programBtn = document.querySelector(".header-menu-item-program");
-const coachesBtn = document.querySelector(".header-menu-item-coaches");
-const reviewBtn = document.querySelector(".header-menu-item-review");
-const contactsBtn = document.querySelector(".header-menu-item-contact");
-
-const aboutSection = document.querySelector(".benefits-section");
-const programSection = document.querySelector(".cource-section");
-const coachesSection = document.querySelector(".coaches-section");
-const reviewSection = document.querySelector(".video-section");
-const contactSection = document.querySelector(".footer");
-
-aboutBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: aboutSection.getBoundingClientRect().top + window.scrollY,
-        behavior: "smooth",
-    });
-});
-programBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: programSection.getBoundingClientRect().top + window.scrollY,
-        behavior: "smooth",
-    });
-});
-coachesBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: coachesSection.getBoundingClientRect().top + window.scrollY,
-        behavior: "smooth",
-    });
-});
-reviewBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: reviewSection.getBoundingClientRect().top + window.scrollY,
-        behavior: "smooth",
-    });
-});
-contactsBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: contactSection.getBoundingClientRect().top + window.scrollY,
-        behavior: "smooth",
-    });
-});
-
-// Animated numbers
-
-$(".counts").each(function (index, element) {
-    var count = $(this),
-        zero = { val: 0 },
-        num = count.data("number"),
-        split = (num + "").split("."),
-        decimals = split.length > 1 ? split[1].length : 0;
-
-    gsap.to(zero, {
-        val: num,
-        duration: 3,
-        scrollTrigger: element,
-        onUpdate: function () {
-            count.text(zero.val.toFixed(decimals));
-        },
-    });
-});
 
 // Marquee coaches
 
